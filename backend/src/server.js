8707a1682dfcfd8deeef90a0211205ffbc5d93c3
anil-shelke <anilshelke912003@gmail.com>
2025-07-14 07:28:39 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,8 +13,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(cors({
-    // origin: 'http://localhost:5173',
-    origin: 'https://video-calling-frontend-sarb.onrender.com',
+    origin: process.env.FRONTEND_URL,
     credentials: true
 }))
 
